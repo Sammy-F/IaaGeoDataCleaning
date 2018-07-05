48b951572be4383d3595375a22c7e34aaa6dbfc2
thytng <tnguye10@macalester.edu>
2018-07-05 12:23:43 -0500

    Changed functions so that they return the child object to be added to the map; made the functions read in the file as a parameter; added an option to plot the points as a cluster or as individual markers.

--- src/main/python/MapTools/experiment.py
+++ src/main/python/MapTools/experiment.py
@@ -1,132 +1,162 @@
-from src.main.python.TableUtils import TableTools
 import folium as foli
 from folium.plugins import MarkerCluster
 import os
 from src.main.python.IaaGeoDataCleaning.verify import Point, gpd, np, pd
 
 class MapTool:
-    def __init__(self, file_path=str(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..', '..',
-                                                                  'tblLocation', 'verified_entries.csv'))),
-                 outfile_type='.csv', loc_col='Location', ctry_col='Country', reg_col='Region',
-                 lat_col='Recorded_Lat', lng_col='Recorded_Lng'):
+    def __init__(self):
+        mapFile = str(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..', '..',
+                                             'resources', 'mapinfo', 'TM_WORLD_BORDERS-0.3.shp')))
+        self.map = self.read_file(mapFile)
 
-        self.tableTools = TableTools(file_path=file_path, loc_col=loc_col, ctry_col=ctry_col,
-                                     reg_col=reg_col, lat_col=lat_col, lng_col=lng_col)
-
-    def plot_point(self, lat, lng, desc=None):
-        m = foli.Map(location=[lat, lng], zoom_start=5)
-        if desc:
-            marker = foli.Marker([lat, lng], popup=desc, icon=foli.Icon(prefix='fa', icon='circle', icon_color='white'))
+    def read_file(self, file_path):
+        """
+        Reads in csv or excel file. Raises an error if a different file type was entered.
+        :param file_path: .csv or .xlsx
+        :return: a pandas data frame.
+        """
+        if file_path.endswith('.xlsx'):
+            data = pd.read_excel(file_path)
+        elif file_path.endswith('.csv'):
+            data = pd.read_csv(file_path)
         else:
-            marker = foli.Marker([lat, lng], icon=foli.Icon(prefix='fa', icon='circle', icon_color='white'))
-        m.add_child(marker)
-        return m
+            raise TypeError('Support is only available for .xlsx and .csv files.')
+        return data
 
-    def plot_all_stations(self):
-        m = foli.Map(location=[0, 0], zoom_start=2)
+    def check_columns(self, df, cols):
+        if not isinstance(cols, set):
+            cols = set(cols)
+        if cols.issubset(df.columns):
+            return True
+        else:
+            raise KeyError('Column names not found in data frame.')
 
-        lat_list = list(self.tableTools.df[self.tableTools.lat_col])
-        lng_list = list(self.tableTools.df[self.tableTools.lng_col])
-        coord_list = [[lat_list[i], lng_list[i]] for i in range(len(lat_list))]
-        loc_list = list(self.tableTools.df[self.tableTools.loc_col])
-        ctry_list = list(self.tableTools.df[self.tableTools.ctry_col])
-        popup_list = [foli.Popup(loc_list[i] + ', ' + ctry_list[i], parse_html=True) for i in range(len(loc_list))]
+    def clean_dataframe(self, infile, cols):
+        df = self.read_file(infile)
+        if self.check_columns(df, cols):
+            return df
 
-        cluster = MarkerCluster(name="Plant Breeding Station Cluster", locations=coord_list, popups=popup_list)
-        m.add_child(cluster)
+    def create_map(self, center=(0, 0), zoom=2):
+        return foli.Map(location=center, zoom_start=zoom)
 
-        return m
+    def plot_point(self, lat, lng, desc=None, clr='blue'):
+        if desc:
+            marker = foli.Marker((lat, lng), popup=foli.Popup(desc, parse_html=True),
+                                 icon=foli.Icon(prefix='fa', color=clr, icon='circle', icon_color='white'))
+        else:
+            marker = foli.Marker((lat, lng), icon=foli.Icon(prefix='fa', color=clr, icon='circle', icon_color='white'))
+        return marker
 
-    def plot_no_country(self):
-        m = foli.Map(location=[0, 0], zoom_start=2)
+    def plot_all_stations(self, infile, loc_col, ctry_col, lat_col, lng_col, clr='blue', as_cluster=True):
+        df = self.clean_dataframe(infile, {loc_col, ctry_col, lat_col, lng_col})
 
-        locations = []
+        loc_list = list(df[loc_col])
+        ctry_list = list(df[ctry_col])
+        lat_list = list(df[lat_col])
+        lng_list = list(df[lng_col])
+
+        if as_cluster:
+            popups = [foli.Popup('%s, %s' % (loc_list[i], ctry_list[i]), parse_html=True) for i in range(len(loc_list))]
+            coords = [(lat_list[i], lng_list[i]) for i in range(len(lat_list))]
+            icons = [foli.Icon(prefix='fa', color=clr, icon='circle', icon_color='white') for i in range(len(df))]
+
+            cluster = MarkerCluster(locations=coords, icons=icons, popups=popups)
+            return cluster
+        else:
+            markers = [self.plot_point(lat=lat_list[i], lng=lng_list[i], desc='%s, %s' % (loc_list[i], ctry_list[i]),
+                                       clr=clr) for i in range(len(df))]
+            return markers
+
+    def plot_potential_errors(self, infile, loc_col, ctry_col, lat_col, lng_col, clr='lightred', as_cluster=False):
+        df = self.clean_dataframe(infile, {loc_col, ctry_col, lat_col, lng_col})
+
+        coords = []
         popups = []
         icons = []
 
-        for (index, row) in self.tableTools.df.iterrows():
-            if not (np.isnan(row[self.tableTools.lat_col]) or np.isnan(row[self.tableTools.lng_col])):
-                lat = row[self.tableTools.lat_col]
-                lng = row[self.tableTools.lng_col]
-                if pd.isnull(row[self.tableTools.loc_col]):
+        markers = []
+
+        for (index, row) in df.iterrows():
+            if pd.notnull(row[lat_col]) and pd.notnull(row[lng_col]):
+                lat = row[lat_col]
+                lng = row[lng_col]
+                if pd.isnull(row[loc_col]):
                     loc = ''
                 else:
-                    loc = row[self.tableTools.loc_col]
-
-                if pd.isnull(row[self.tableTools.ctry_col]):
+                    loc = row[loc_col]
+                if pd.isnull(row[ctry_col]):
                     ctry = ''
                 else:
-                    ctry = row[self.tableTools.ctry_col]
+                    ctry = row[ctry_col]
 
                 point = Point(np.array([lng, lat]))
-                filter = self.tableTools.validator.map['geometry'].contains(point)
-                mLoc = self.tableTools.validator.map.loc[filter, 'ISO3']
+                filtered = self.map['geometry'].contains(point)
+                mLoc = self.map.loc[filtered, 'ISO3']
 
                 if len(list(mLoc)) == 0:
-                    locations.append((lat, lng))
-                    popups.append(foli.Popup(loc + ', ' + ctry, parse_html=True))
-                    icons.append(foli.Icon(prefix='fa', icon='circle', icon_color='white', color='red'))
+                    if as_cluster:
+                        coords.append((lat, lng))
+                        popups.append(foli.Popup('%s, %s' % (loc, ctry), parse_html=True))
+                        icons.append(foli.Icon(prefix='fa', color=clr, icon='circle', icon_color='white'))
+                    else:
+                        markers.append(self.plot_point(lat=lat, lng=lng, desc='%s, %s' % (loc, ctry), clr=clr))
 
-        cluster = MarkerCluster(name="Out of Border Stations", locations=locations, icons=icons, popups=popups)
-        m.add_child(cluster)
+        if as_cluster:
+            return MarkerCluster(locations=coords, popups=popups, icons=icons)
+        else:
+            return markers
 
-        return m, locations
+    def plot_type(self, infile, type, type_col, loc_col, ctry_col, lat_col, lng_col, clr='blue', as_cluster=False):
+        df = self.clean_dataframe(infile, {type_col, loc_col, ctry_col, lat_col, lng_col})
 
-    def plot_geocoded_locations(self):
-        m = foli.Map(location=[0, 0], zoom_start=2)
+        coords = []
+        popups = []
+        icons = []
 
-        for (index, row) in self.tableTools.df.iterrows():
-            if row['Type'] == 'no lat/lng entered / incorrect lat/lng - geocoded location':
-                lat = row[self.tableTools.lat_col]
-                lng = row[self.tableTools.lng_col]
-                loc = row[self.tableTools.loc_col]
-                ctry = row[self.tableTools.ctry_col]
+        markers = []
 
-                marker = foli.Marker(location=[lat, lng], popup=foli.Popup(loc + ', ' + ctry, parse_html=True))
-                m.add_child(marker)
+        for (index, row) in df.iterrows():
+            if row[type_col] == type:
+                if pd.notnull(row[lat_col]) and pd.notnull(row[lng_col]):
+                    lat = row[lat_col]
+                    lng = row[lng_col]
+                    if pd.isnull(row[loc_col]):
+                        loc = ''
+                    else:
+                        loc = row[loc_col]
+                    if pd.isnull(row[ctry_col]):
+                        ctry = ''
+                    else:
+                        ctry = row[ctry_col]
 
-        return m
+                    if as_cluster:
+                        coords.append((lat, lng))
+                        popups.append(foli.Popup('%s, %s' % (loc, ctry), parse_html=True))
+                        icons.append(foli.Icon(prefix='fa', color=clr, icon='circle', icon_color='white'))
+                    else:
+                        markers.append(self.plot_point(lat, lng, '%s, %s' % (loc, ctry), clr))
 
-    def plot_pair(self, index, orig_lat_col, orig_lng_col, rec_lat_col, rec_lng_col):
-        m = foli.Map(location=[0, 0], zoom_start=2)
+        if as_cluster:
+            return MarkerCluster(locations=coords, popups=popups, icons=icons)
+        else:
+            return markers
 
-        orig_lat = self.tableTools.df.loc[index, orig_lat_col]
-        orig_lng = self.tableTools.df.loc[index, orig_lng_col]
-        rec_lat = self.tableTools.df.loc[index, rec_lat_col]
-        rec_lng = self.tableTools.df.loc[index, rec_lng_col]
+    def plot_pair_in_df(self, infile, index, lat0_col, lng0_col, lat1_col, lng1_col, clr0='lightblue', clr1='darkblue'):
+        df = self.clean_dataframe(infile, {lat0_col, lng0_col, lat1_col, lng1_col})
 
-        loc = self.tableTools.df.loc[index, self.tableTools.loc_col]
-        ctry = self.tableTools.df.loc[index, self.tableTools.ctry_col]
+        if index < len(df):
+            coords0 = (df.loc[index, lat0_col], df.loc[index, lng0_col])
+            coords1 = (df.loc[index, lat1_col], df.loc[index, lng1_col])
 
-        orig_loc = foli.Marker(location=[orig_lat, orig_lng],
-                               popup=foli.Popup(loc + ', ' + ctry, parse_html=True),
-                               icon=foli.Icon(prefix='fa', icon='circle', icon_color='white', color='red'))
-        rec_loc = foli.Marker(location=[rec_lat, rec_lng],
-                              popup=foli.Popup(loc + ', ' + ctry, parse_html=True),
-                              icon=foli.Icon(prefix='fa', icon='circle', icon_color='white', color='green'))
+            return self.plot_pair(coords0, coords1, clr0, clr1)
+        else:
+            raise KeyError('Index out of range.')
 
-        m.add_child(orig_loc)
-        m.add_child(rec_loc)
+    def plot_pair(self, coords0, coords1, clr0='lightblue', clr1='darkblue'):
+        marker0 = self.plot_point(lat=coords0[0], lng=coords0[1], desc=str(coords0), clr=clr0)
+        marker1 = self.plot_point(lat=coords1[0], lng=coords1[1], desc=str(coords1), clr=clr1)
 
-        return m
-
-    def compare_points(self, first_coord, second_coord):
-        m = foli.Map(location=[0, 0], zoom_start=2)
-
-        first_loc = foli.Marker(location=first_coord,
-                                popup=foli.Popup('(%s, %s)' % (first_coord[0], first_coord[1]), parse_html=True),
-                                icon=foli.Icon(prefix='fa', icon='circle', icon_color='white', color='green'))
-        second_loc = foli.Marker(location=second_coord,
-                                 popup=foli.Popup('(%s, %s)' % (second_coord[0], second_coord[1]), parse_html=True),
-                                 icon=foli.Icon(prefix='fa', icon='circle', icon_color='white', color='blue'))
-        m.add_child(first_loc)
-        m.add_child(second_loc)
-
-        return m
-
-
-
-
+        return marker0, marker1
 
 
 
