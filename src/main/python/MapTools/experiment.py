05df72e5f04f808f196c6a2c8f7c10d76810e02b
thytng <tnguye10@macalester.edu>
2018-07-06 13:10:27 -0500

    Documented code

--- src/main/python/MapTools/experiment.py
+++ src/main/python/MapTools/experiment.py
@@ -6,11 +6,14 @@ import numpy as np
 import geopandas as gpd
 from shapely.geometry import Point
 import math
-# TODO: altair, side by side maps, plot stations in a country? continent?
+# TODO: plot all stations whose coordinates do not match the country.
 
 
 class MapTool:
     def __init__(self):
+        """
+        Initializes a tool for mapping data points as markers using the folium package.
+        """
         mapFile = str(path.abspath(path.join(path.dirname(__file__), '..', '..', '..', '..',
                                              'resources', 'mapinfo', 'TM_WORLD_BORDERS-0.3.shp')))
         self.map = gpd.read_file(mapFile)
@@ -30,6 +33,12 @@ class MapTool:
         return data
 
     def check_columns(self, df, cols):
+        """
+        Checks to see whether the columns exist in the dataframe.
+        :param df: pandas dataframe.
+        :param cols: a tuple, list, or set of column names.
+        :return:
+        """
         if not isinstance(cols, set):
             cols = set(cols)
         if cols.issubset(df.columns):
@@ -38,6 +47,12 @@ class MapTool:
             raise KeyError('Column names not found in data frame.')
 
     def clean_dataframe(self, infile, cols):
+        """
+        Confirms that a file can be used as a dataframe.
+        :param infile: filepath to the data.
+        :param cols: a tuple, list, or set of column names.
+        :return: a pandas dataframe of the file.
+        """
         df = self.read_file(infile)
         if self.check_columns(df, cols):
             return df
@@ -53,6 +68,14 @@ class MapTool:
         return loc, ctry
 
     def haversine(self, lat0, lng0, lat1, lng1):
+        """
+        Calculates the distance between two coordinates using the haversine formula.
+        :param lat0:
+        :param lng0:
+        :param lat1:
+        :param lng1:
+        :return: the distance in km.
+        """
         rlat0 = math.radians(lat0)
         rlat1 = math.radians(lat1)
         dlat = rlat1 - rlat0
@@ -63,6 +86,14 @@ class MapTool:
         return 6371 * c
 
     def plot_point(self, lat, lng, desc=None, clr='blue'):
+        """
+        Creates a single marker.
+        :param lat:
+        :param lng:
+        :param desc:
+        :param clr:
+        :return:
+        """
         if desc:
             marker = Marker((lat, lng), popup=Popup(desc, parse_html=True),
                             icon=Icon(prefix='fa', color=clr, icon='circle', icon_color='white'))
@@ -71,6 +102,17 @@ class MapTool:
         return marker
 
     def plot_all_stations(self, infile, loc_col, ctry_col, lat_col, lng_col, clr='blue', as_cluster=True):
+        """
+        Plots all data points in the file.
+        :param infile: filepath to the data.
+        :param loc_col: name of the location column.
+        :param ctry_col: name of the country column.
+        :param lat_col: name of the latitude column.
+        :param lng_col: name of the longitude column.
+        :param clr: color of the markers.
+        :param as_cluster: boolean value - create the markers as a cluster or not.
+        :return: a list of markers if as_cluster is False and a MarkerCluster otherwise.
+        """
         df = self.clean_dataframe(infile, {loc_col, ctry_col, lat_col, lng_col})
 
         if as_cluster:
@@ -93,6 +135,17 @@ class MapTool:
         return markers
 
     def plot_potential_errors(self, infile, loc_col, ctry_col, lat_col, lng_col, clr='lightred', as_cluster=False):
+        """
+        Plots all data points whose coordinates do not fall within any country.
+        :param infile:
+        :param loc_col:
+        :param ctry_col:
+        :param lat_col:
+        :param lng_col:
+        :param clr:
+        :param as_cluster: boolean value - create the markers as a cluster or not.
+        :return: a list of markers if as_cluster is False and a MarkerCluster otherwise.
+        """
         df = self.clean_dataframe(infile, {loc_col, ctry_col, lat_col, lng_col})
 
         if as_cluster:
@@ -120,6 +173,19 @@ class MapTool:
         return markers
 
     def plot_condition(self, infile, condition, cnd_col, loc_col, ctry_col, lat_col, lng_col, clr='blue', as_cluster=False):
+        """
+        Plots all of the data points that meet the specified condition.
+        :param infile:
+        :param condition: condition of the entries (currently works best with strings).
+        :param cnd_col: name of the column to be evaluated.
+        :param loc_col:
+        :param ctry_col:
+        :param lat_col:
+        :param lng_col:
+        :param clr: color of the markers.
+        :param as_cluster: boolean value - create the markers as a cluster or not.
+        :return: a list of markers if as_cluster is False and a MarkerCluster otherwise.
+        """
         df = self.clean_dataframe(infile, {cnd_col, loc_col, ctry_col, lat_col, lng_col})
 
         if as_cluster:
@@ -143,6 +209,18 @@ class MapTool:
         return markers
 
     def plot_pair_in_df(self, infile, index, lat0_col, lng0_col, lat1_col, lng1_col, clr0='lightblue', clr1='darkblue'):
+        """
+        Plots a data point if it contains two different coordinates for comparison.
+        :param infile:
+        :param index:
+        :param lat0_col:
+        :param lng0_col:
+        :param lat1_col:
+        :param lng1_col:
+        :param clr0:
+        :param clr1:
+        :return: the data points as Marker objects.
+        """
         df = self.clean_dataframe(infile, {lat0_col, lng0_col, lat1_col, lng1_col})
 
         if index < len(df):
@@ -154,12 +232,34 @@ class MapTool:
             raise KeyError('Index out of range.')
 
     def plot_pair(self, coords0, coords1, clr0='lightblue', clr1='darkblue'):
+        """
+        Plots two coordinates for comparison.
+        :param coords0:
+        :param coords1:
+        :param clr0:
+        :param clr1:
+        :return: two Marker objects.
+        """
         marker0 = self.plot_point(lat=coords0[0], lng=coords0[1], desc=str(coords0), clr=clr0)
         marker1 = self.plot_point(lat=coords1[0], lng=coords1[1], desc=str(coords1), clr=clr1)
 
         return marker0, marker1
 
     def plot_within_range(self, infile, center, radius, loc_col, ctry_col, lat_col, lng_col, desc0=None, clr0='blue', clr1='lightblue'):
+        """
+        Plots all data points within the range of the given center and radius.
+        :param infile:
+        :param center:
+        :param radius: in km.
+        :param loc_col:
+        :param ctry_col:
+        :param lat_col:
+        :param lng_col:
+        :param desc0:
+        :param clr0:
+        :param clr1:
+        :return: all of the data points as Marker objects.
+        """
         df = self.clean_dataframe(infile, {loc_col, ctry_col, lat_col, lng_col})
         if not desc0:
             desc0 = str(tuple(center))
@@ -179,6 +279,19 @@ class MapTool:
         return markers
 
     def plot_within_station(self, infile, index, radius, loc_col, ctry_col, lat_col, lng_col, clr0='blue', clr1='lightblue'):
+        """
+        Plots all of the data points within the radius of the specified data point.
+        :param infile:
+        :param index: index of the entry.
+        :param radius:
+        :param loc_col:
+        :param ctry_col:
+        :param lat_col:
+        :param lng_col:
+        :param clr0: color of the given data point.
+        :param clr1: color of the other points.
+        :return: all of the data points as Marker objects.
+        """
         df = self.clean_dataframe(infile, {loc_col, ctry_col, lat_col, lng_col})
         if index < len(df):
             coords = (df.loc[index, lat_col], df.loc[index, lng_col])
