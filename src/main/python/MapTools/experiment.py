0ca69b2e12dd5fc1b68576861a6d6881af69349d
thytng <tnguye10@macalester.edu>
2018-07-06 12:44:58 -0500

    Modified the handling of creating clusters, added functions to plot within a range/station

--- src/main/python/MapTools/experiment.py
+++ src/main/python/MapTools/experiment.py
@@ -5,9 +5,8 @@ import pandas as pd
 import numpy as np
 import geopandas as gpd
 from shapely.geometry import Point
-
-
-# TODO: altair, side by side maps, plot points within a range
+import math
+# TODO: altair, side by side maps, plot stations in a country? continent?
 
 
 class MapTool:
@@ -46,6 +45,23 @@ class MapTool:
     def create_map(self, center=(0, 0), zoom=2):
         return Map(location=center, zoom_start=zoom)
 
+    def format_popup(self, loc, ctry):
+        if not loc:
+            loc = ''
+        if not ctry:
+            ctry = ''
+        return loc, ctry
+
+    def haversine(self, lat0, lng0, lat1, lng1):
+        rlat0 = math.radians(lat0)
+        rlat1 = math.radians(lat1)
+        dlat = rlat1 - rlat0
+        dlng = math.radians(lng1 - lng0)
+
+        a = math.pow(math.sin(dlat/2), 2) + math.cos(rlat0) * math.cos(rlat1) * math.pow(math.sin(dlng/2), 2)
+        c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+        return 6371 * c
+
     def plot_point(self, lat, lng, desc=None, clr='blue'):
         if desc:
             marker = Marker((lat, lng), popup=Popup(desc, parse_html=True),
@@ -57,96 +73,74 @@ class MapTool:
     def plot_all_stations(self, infile, loc_col, ctry_col, lat_col, lng_col, clr='blue', as_cluster=True):
         df = self.clean_dataframe(infile, {loc_col, ctry_col, lat_col, lng_col})
 
-        loc_list = list(df[loc_col])
-        ctry_list = list(df[ctry_col])
-        lat_list = list(df[lat_col])
-        lng_list = list(df[lng_col])
-
         if as_cluster:
-            popups = [Popup('%s, %s' % (loc_list[i], ctry_list[i]), parse_html=True) for i in range(len(loc_list))]
-            coords = [(lat_list[i], lng_list[i]) for i in range(len(lat_list))]
-            icons = [Icon(prefix='fa', color=clr, icon='circle', icon_color='white') for i in range(len(df))]
-
-            cluster = MarkerCluster(locations=coords, icons=icons, popups=popups)
-            return cluster
+            markers = MarkerCluster()
         else:
-            markers = [self.plot_point(lat=lat_list[i], lng=lng_list[i], desc='%s, %s' % (loc_list[i], ctry_list[i]),
-                                       clr=clr) for i in range(len(df))]
-            return markers
-
-    def plot_potential_errors(self, infile, loc_col, ctry_col, lat_col, lng_col, clr='lightred', as_cluster=False):
-        df = self.clean_dataframe(infile, {loc_col, ctry_col, lat_col, lng_col})
-
-        coords = []
-        popups = []
-        icons = []
-
-        markers = []
+            markers = []
 
         for (index, row) in df.iterrows():
             if pd.notnull(row[lat_col]) and pd.notnull(row[lng_col]):
                 lat = row[lat_col]
                 lng = row[lng_col]
-                if pd.isnull(row[loc_col]):
-                    loc = ''
+                location = self.format_popup(row[loc_col], row[ctry_col])
+
+                marker = self.plot_point(lat=lat, lng=lng, desc='%s, %s' % (location[0], location[1]), clr=clr)
+                if as_cluster:
+                    marker.add_to(markers)
                 else:
-                    loc = row[loc_col]
-                if pd.isnull(row[ctry_col]):
-                    ctry = ''
-                else:
-                    ctry = row[ctry_col]
+                    markers.append(marker)
+
+        return markers
+
+    def plot_potential_errors(self, infile, loc_col, ctry_col, lat_col, lng_col, clr='lightred', as_cluster=False):
+        df = self.clean_dataframe(infile, {loc_col, ctry_col, lat_col, lng_col})
+
+        if as_cluster:
+            markers = MarkerCluster()
+        else:
+            markers = []
+
+        for (index, row) in df.iterrows():
+            if pd.notnull(row[lat_col]) and pd.notnull(row[lng_col]):
+                lat = row[lat_col]
+                lng = row[lng_col]
+                location = self.format_popup(row[loc_col], row[ctry_col])
 
                 point = Point(np.array([lng, lat]))
                 filtered = self.map['geometry'].contains(point)
                 mLoc = self.map.loc[filtered, 'ISO3']
 
                 if len(list(mLoc)) == 0:
+                    marker = self.plot_point(lat=lat, lng=lng, desc='%s, %s' % (location[0], location[1]), clr=clr)
                     if as_cluster:
-                        coords.append((lat, lng))
-                        popups.append(Popup('%s, %s' % (loc, ctry), parse_html=True))
-                        icons.append(Icon(prefix='fa', color=clr, icon='circle', icon_color='white'))
+                        marker.add_to(markers)
                     else:
-                        markers.append(self.plot_point(lat=lat, lng=lng, desc='%s, %s' % (loc, ctry), clr=clr))
+                        markers.append(marker)
 
-        if as_cluster:
-            return MarkerCluster(locations=coords, popups=popups, icons=icons)
-        else:
-            return markers
+        return markers
 
     def plot_condition(self, infile, condition, cnd_col, loc_col, ctry_col, lat_col, lng_col, clr='blue', as_cluster=False):
         df = self.clean_dataframe(infile, {cnd_col, loc_col, ctry_col, lat_col, lng_col})
 
-        coords = []
-        popups = []
-        icons = []
-
-        markers = []
+        if as_cluster:
+            markers = MarkerCluster()
+        else:
+            markers = []
 
         for (index, row) in df.iterrows():
             if str(row[cnd_col]).lower() == str(condition).lower():
                 if pd.notnull(row[lat_col]) and pd.notnull(row[lng_col]):
                     lat = row[lat_col]
                     lng = row[lng_col]
-                    if pd.isnull(row[loc_col]):
-                        loc = ''
-                    else:
-                        loc = row[loc_col]
-                    if pd.isnull(row[ctry_col]):
-                        ctry = ''
-                    else:
-                        ctry = row[ctry_col]
+                    location = self.format_popup(row[loc_col], row[ctry_col])
 
+                    marker = self.plot_point(lat=lat, lng=lng, desc='%s, %s' % (location[0], location[1]), clr=clr)
                     if as_cluster:
-                        coords.append((lat, lng))
-                        popups.append(Popup('%s, %s' % (loc, ctry), parse_html=True))
-                        icons.append(Icon(prefix='fa', color=clr, icon='circle', icon_color='white'))
+                        marker.add_to(markers)
                     else:
-                        markers.append(self.plot_point(lat, lng, '%s, %s' % (loc, ctry), clr))
+                        markers.append(marker)
 
-        if as_cluster:
-            return MarkerCluster(locations=coords, popups=popups, icons=icons)
-        else:
-            return markers
+        return markers
 
     def plot_pair_in_df(self, infile, index, lat0_col, lng0_col, lat1_col, lng1_col, clr0='lightblue', clr1='darkblue'):
         df = self.clean_dataframe(infile, {lat0_col, lng0_col, lat1_col, lng1_col})
@@ -164,3 +158,31 @@ class MapTool:
         marker1 = self.plot_point(lat=coords1[0], lng=coords1[1], desc=str(coords1), clr=clr1)
 
         return marker0, marker1
+
+    def plot_within_range(self, infile, center, radius, loc_col, ctry_col, lat_col, lng_col, desc0=None, clr0='blue', clr1='lightblue'):
+        df = self.clean_dataframe(infile, {loc_col, ctry_col, lat_col, lng_col})
+        if not desc0:
+            desc0 = str(tuple(center))
+        markers = []
+        for (index, row) in df.iterrows():
+            if pd.notnull(row[lat_col]) and pd.notnull(row[lng_col]):
+                lat = row[lat_col]
+                lng = row[lng_col]
+                d = self.haversine(center[0], center[1], lat, lng)
+
+                if d < radius:
+                    location = self.format_popup(row[loc_col], row[ctry_col])
+                    markers.append(self.plot_point(lat=lat, lng=lng, clr=clr1,
+                                                   desc='%s, %s, %s km' % (location[0], location[1], format(d, '.3f'))))
+
+        markers.append(self.plot_point(lat=center[0], lng=center[1], desc=desc0, clr=clr0))
+        return markers
+
+    def plot_within_station(self, infile, index, radius, loc_col, ctry_col, lat_col, lng_col, clr0='blue', clr1='lightblue'):
+        df = self.clean_dataframe(infile, {loc_col, ctry_col, lat_col, lng_col})
+        if index < len(df):
+            coords = (df.loc[index, lat_col], df.loc[index, lng_col])
+            location = '%s, %s' % (df.loc[index, loc_col], df.loc[index, ctry_col])
+            return self.plot_within_range(infile, coords, radius, loc_col, ctry_col, lat_col, lng_col, location, clr0, clr1)
+        else:
+            raise KeyError('Index out of range.')
