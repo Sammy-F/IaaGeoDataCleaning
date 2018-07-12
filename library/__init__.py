3b830f4d0f5b0c111a4249a3a7c5ccf7a49b0b0c
Sammy-F <sfritsch@macalester.edu>
2018-07-12 13:44:11 -0500

    Updated .gitignore and removed unneeded statements from library __init__

--- library/__init__.py
+++ library/__init__.py
@@ -6,7 +6,3 @@
 #######################################################
 __version__ = '0.1.1'
 __all__ = ['CleaningUtils', 'ConnectionUtils', 'MapTools', 'TableUtils']
-from library.CleaningUtils import GeoDataCorrector
-from library.ConnectionUtils import DatabaseConnector, Table
-from library.MapTools import MapTool
-from library.TableUtils import TableTools
