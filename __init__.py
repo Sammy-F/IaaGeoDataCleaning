c4c791db69bb39615281916e7e2228c84fb5cee1
Sammy-F <sfritsch@macalester.edu>
2018-07-19 16:03:38 -0500

    rebuilt docs

--- __init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-#######################################################
-#copyright (c) IAA
-#authors: Samantha Fritsche, Thy Nguyen
-#######################################################
-__version__ = '0.1.1'
-__all__ = ['IaaGeoDataCleaning']
